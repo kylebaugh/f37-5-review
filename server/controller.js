900eb6fec81115b36be2f78aad18e747c4ad6fc8
kylebaugh <kylebaugh1@gmail.com>
2023-03-31 10:41:18 -0600

    created weapons table, seeded database

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,18 @@
 
+const Sequelize = require('sequelize')
+
+require('dotenv').config()
+
+const {CONNECTION_STRING} = process.env
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
 
 module.exports = {
     seed: (req, res) => {
@@ -13,8 +27,14 @@ module.exports = {
                 hp INT NOT NULL,
                 type VARCHAR NOT NULL
             );
-
-            --create weapons table--
+            
+            CREATE TABLE weapons(
+                id SERIAL PRIMARY KEY,
+                name VARCHAR NOT NULL, 
+                power INT NOT NULL,
+                owner_id INT REFERENCES fighters(id)
+            );
+            
         `)
         .then(() => {
             console.log('DB seeded!')
