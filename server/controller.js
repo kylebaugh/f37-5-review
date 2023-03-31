ef24f75480d458b9459ad9c3142b01cd4b130a52
kylebaugh <kylebaugh1@gmail.com>
2023-03-31 11:45:52 -0600

    completed review

--- server/controller.js
+++ server/controller.js
@@ -45,5 +45,117 @@ module.exports = {
             console.log(err)
             res.status(500).send(err)
         })
+    },
+
+    createFighter: (req, res) => {
+
+        const {name, power, hp, type} = req.body
+
+        sequelize.query(`
+            INSERT INTO fighters
+            (name, power, hp, type)
+            VALUES
+            ('${name}', ${power}, ${hp}, '${type}')
+            RETURNING*;
+        `)
+            .then((dbRes) => {
+                console.log('createFighter success!')
+                res.status(200).send(dbRes[0])
+            })
+            .catch((theseHands) => {
+                console.log('Error with createFighter')
+                console.log(theseHands)
+                res.status(500).send(theseHands)
+            })
+    }, 
+
+    getFightersList: (req, res) => {
+        // get names and ids from fighters table
+        // handle response with .then and .catch
+
+        sequelize.query(`
+            SELECT name, id FROM fighters;
+        `)
+            .then((dbRes) => {
+                console.log('getFightersList success!')
+                res.status(200).send(dbRes[0])
+            })
+            .catch((theseHands) => {
+                console.log('Error with getFightersList')
+                console.log(theseHands)
+                res.status(500).send(theseHands)
+            })
+    }, 
+
+    createWeapon: (req, res) => {
+
+        const {name, power, owner} = req.body
+
+        sequelize.query(`
+            INSERT INTO weapons
+            (name, power, owner_id)
+            VALUES
+            ('${name}', ${power}, ${owner})
+            RETURNING *;
+        `)
+            .then((dbRes) => {
+                console.log('createWeapon success!')
+                res.status(200).send(dbRes[0])
+            })
+            .catch((theseHands) => {
+                console.log('Error with createWeapon')
+                console.log(theseHands)
+                res.status(500).send(theseHands)
+            })
+    },
+
+    getFightersWeapons: (req, res) => {
+        // select all data from fighters table
+        // Join weapons table with fighters table - specifically on id
+        // Setup aliases for everything but hp and type
+        // handle our response
+
+        sequelize.query(`
+            SELECT
+                fighters.id AS fighter_id,
+                fighters.name AS fighter,
+                fighters.power AS fighter_power,
+                hp,
+                type,
+                weapons.id AS weapon_id,
+                weapons.name AS weapon,
+                weapons.power AS weapon_power
+            FROM fighters
+            JOIN weapons
+            ON fighters.id = weapons.owner_id;
+        `)
+            .then((dbRes) => {
+                console.log('getFightersWeapons success!')
+                res.status(200).send(dbRes[0])
+            })
+            .catch((theseHands) => {
+                console.log('Error with getFightersWeapons')
+                console.log(theseHands)
+                res.status(500).send(theseHands)
+            })
+
+    },
+
+    deleteWeapon: (req, res) => {
+        const {id} = req.params
+
+        sequelize.query(`
+            DELETE FROM weapons
+            WHERE id = ${id};
+        `)
+            .then((dbRes) => {
+                console.log('deleteWeapon success!')
+                res.status(200).send(dbRes[0])
+            })
+            .catch((theseHands) => {
+                console.log('Error with deleteWeapon')
+                console.log(theseHands)
+                res.status(500).send(theseHands)
+            })
     }
 }
--- server/index.js
+++ server/index.js
@@ -18,10 +18,10 @@ const {
 } = require('./controller.js')
 
 app.post('/seed', seed)
-// app.post('/fighter', createFighter)
-// app.post('/weapon', createWeapon)
-// app.get('/fighters-list', getFightersList)
-// app.get('/fighters-and-weapons', getFightersWeapons)
-// app.delete('/weapon/:id', deleteWeapon)
+app.post('/fighter', createFighter)
+app.post('/weapon', createWeapon)
+app.get('/fighters-list', getFightersList)
+app.get('/fighters-and-weapons', getFightersWeapons)
+app.delete('/weapon/:id', deleteWeapon)
 
 app.listen(PORT, () => console.log(`up on ${PORT}`))
